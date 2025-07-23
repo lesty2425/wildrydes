4f9053d23e7f1135da55523703df06ad34dbf750
lesty2425 <41571556+lesty2425@users.noreply.github.com>
2025-07-23 12:54:03 +0800

    Update config.js
    added in client secret

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@ window._config = {
     cognito: {
         userPoolId: 'ap-southeast-1_ktevltSGm', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '439pv4fr3lqoab0dhcka4gd6rh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-1' // e.g. us-east-2
+        region: 'ap-southeast-1', // e.g. us-east-2
+        secret: 'r862dcib2dnl0j48k8qfspoegsjlteotff0sl5ahpi8cs2cdblo'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
