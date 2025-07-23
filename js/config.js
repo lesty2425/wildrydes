069b971a39cc4638a90bb7f6d4050d2c57a2bf3a
lesty2425 <41571556+lesty2425@users.noreply.github.com>
2025-07-23 12:48:14 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_ktevltSGm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '439pv4fr3lqoab0dhcka4gd6rh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
